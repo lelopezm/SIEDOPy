a1485c19c64af08324f9558b935cc411291f3e6b
leon55490 <leon55490@gmail.com>
2023-11-17 14:10:28 -0500

    configuracion del main.py

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from methods.euler import Euler
-import matplotlib.pyplot as plt
+import subprocess
+import os
+import sys
+import tkinter as tk
+from tkinter import messagebox
 
-def main():
-    ''' Application initializer. '''
-    modelo: Euler = Euler()
-    modelo.solve()
+def run_euler_interface():
+    try:
+        # Ruta al script que deseas ejecutar
+        script_path = os.path.join(sys.path[0], 'ScriptsPython', 'CodigosPVI', 'InterfazODE.py')
+        
+        # Verifica si el archivo existe antes de intentar ejecutarlo
+        if os.path.isfile(script_path):
+            # Ejecuta el script
+            subprocess.run(['python', script_path], check=True)
+        else:
+            messagebox.showerror("Error", "El archivo InterfazODE.py no se encontró.")
+    except subprocess.CalledProcessError as e:
+        messagebox.showerror("Error", f"Hubo un error al ejecutar InterfazODE.py: {e}")
 
-    print(
-        f'Euler Method X: {modelo.X[-1]}'
-    )
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Menú de Aplicación de Interfaces")
 
-    plt.plot(modelo.T, modelo.X, color='blue', linewidth=2, linestyle='-', marker='o', markersize=5, markerfacecolor='red', markeredgewidth=1, markeredgecolor='black')
-    plt.title('ODE Solution')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Value of X')
-    plt.grid(True)
+# Configurar tamaño de la ventana
+root.geometry("300x200")
 
-    plt.show()
+# Crear un botón que al hacer clic ejecute la interfaz Euler
+euler_button = tk.Button(root, text="Euler", command=run_euler_interface)
+euler_button.pack(pady=20)
 
-if __name__ == '__main__':
-    main()
+# Iniciar el bucle principal de Tkinter
+root.mainloop()
