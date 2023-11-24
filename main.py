37fa2e63fbee431317a83160ea6338d11f806069
leon55490 <leon55490@gmail.com>
2023-11-24 10:54:02 -0500

    Agregacion Boton del main

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ from tkinter import messagebox
 
 def salir_de_la_aplicacion():
     root.destroy()
+    
+def run_first_interface():
+    try:
+        # Ruta al script que deseas ejecutar
+        script_path = os.path.join(sys.path[0], 'ScriptsPython', 'primero.py')
+        
+        # Verifica si el archivo existe antes de intentar ejecutarlo
+        if os.path.isfile(script_path):
+            # Ejecuta el script
+            subprocess.run(['python', script_path], check=True)
+        else:
+            messagebox.showerror("Error", "El archivo primero.py no se encontró.")
+    except subprocess.CalledProcessError as e:
+        messagebox.showerror("Error", f"Hubo un error al ejecutar primero.py: {e}")
+
 
 def run_euler_interface():
     try:
@@ -34,6 +49,11 @@ root.geometry("300x200")
 # Crear un botón que al hacer clic ejecute la interfaz Solución Metodos Numericos
 interfaz_button = ttk.Button(root, text="Solución Metodos Numericos", command=run_euler_interface)
 interfaz_button.pack(pady=20)
+
+# Crear un botón que al hacer clic ejecute la interfaz de crecimiento exponencial
+first_interface_button = ttk.Button(root, text="Interfaz de Crecimiento Exponencial", command=run_first_interface)
+first_interface_button.pack(pady=10)
+
 # Crear un botón que al hacer clic salga de la interfaz 
 salir_button = ttk.Button(root, text="Salir", command=salir_de_la_aplicacion)
 salir_button.pack(pady=20)
